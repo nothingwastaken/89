5b28438a45f11ad3d3fdce39e2f411420a46487d
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2022-01-28 14:47:00 +1100

    Update maths_game.js

--- maths_game.js
+++ maths_game.js
@@ -29,3 +29,39 @@ document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
 }
 
+
+player_answer = "player2"
+player_question = "player1"
+
+function check(){
+   answer = document.getElementById("input_check_box").value;
+    if (answer == a_a){
+        if (player_answer == "player1")
+        {
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else
+        {
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+    }
+    }
+    if(player_question == "player1"){
+        player_question = "player2"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else{
+        player_question = "player1"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+    if(player_answer == "player1"){
+        player_answer = "player2"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else{
+        player_answer = "player1"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+    document.getElementById("output").innerHTML = "";
+    }
